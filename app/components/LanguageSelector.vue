3b47d732ebc91bea45b740274efab3124acbed47
AxelSorensen <axelsorensenwork@gmail.com>
2026-01-02 19:18:57 -0500

    feat: improve language selector UI and ensure modal component closes correctly

--- app/components/LanguageSelector.vue
+++ app/components/LanguageSelector.vue
@@ -6,25 +6,48 @@
     >
       <span>{{ getFlag(targetLanguage.id) }}</span>
       <span>{{ targetLanguage.name }}</span>
-      <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+      <svg
+        class="w-4 h-4"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M19 9l-7 7-7-7"
+        ></path>
       </svg>
     </button>
 
-    <Modal :is-open="showModal" title="Select Language" @close="showModal = false">
+    <Modal
+      :is-open="showModal"
+      title="Select Language"
+      @close="showModal = false"
+    >
       <div class="space-y-2">
         <button
           v-for="lang in languages"
           :key="lang.id"
           @click="selectLanguage(lang)"
           class="w-full text-left px-4 py-3 hover:bg-gray-100 rounded-lg flex items-center gap-3 transition-colors"
-          :class="{ 'bg-blue-50 border border-blue-200': targetLanguage.id === lang.id }"
+          :class="{
+            'bg-blue-50 border border-blue-200': targetLanguage.id === lang.id,
+          }"
         >
           <span class="text-xl">{{ getFlag(lang.id) }}</span>
           <span class="font-medium">{{ lang.name }}</span>
-          <span v-if="targetLanguage.id === lang.id" class="ml-auto text-blue-500">
+          <span
+            v-if="targetLanguage.id === lang.id"
+            class="ml-auto text-blue-500"
+          >
             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-              <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+              <path
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
             </svg>
           </span>
         </button>
